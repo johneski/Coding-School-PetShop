580ac3738cb2b8fd76558adfcbe8d0e966e4b29d
paoka <paoka@LAPTOP-1016MKVC>
2022-03-17 00:20:56 +0200

    create pets and pet manager

---
+++ PetShop/PetShopLibrary/Pet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+
+public enum AnimalType
+{
+    Bird,
+    Reptilian,
+    Mammal
+
+}
+
+
+public enum PetFoodType
+{
+    CatFood,
+    DogFood,
+    FishFood
+}
+
+public enum PetStatus
+{
+    OK,
+    Unhealthy,
+    Recovering
+}
+
+internal interface IPet
+{
+    PetFoodType Type { get; set; }
+    PetStatus Status { get; set; }
+    AnimalType AnimalType { get; set; }
+    string Breed { get; set; }
+
+}
+
+public class Pet : IPet 
+{
+    public PetFoodType Type { get; set; }
+    public PetStatus Status { get; set; }
+    public AnimalType AnimalType { get; set; }
+    public string Breed { get; set; }
+    
+
+    public Pet()
+	{
+	}
+}
