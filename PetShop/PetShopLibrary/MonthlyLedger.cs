c4cd0d14afc69d737d636fbd07a9a55b318716dc
Giannis Eskioglou <johneski65@gmail.com>
2022-03-17 01:07:02 +0200

    TransactionManager initialization and some corrections

--- PetShop/PetShopLibrary/MonthlyLedger.cs
+++ PetShop/PetShopLibrary/MonthlyLedger.cs
@@ -14,23 +14,24 @@ namespace PetShopLibrary
         public decimal Expences { get; set; }
 
         public decimal Total { get; set; }
-
-        public class MonthlyLedger : IMonthlyLedger
+    }
+    public class MonthlyLedger : IMonthlyLedger
         {
-            public int Year { get; set; }
-            public int Month { get; set; }
-            public decimal Income { get; set; }
-            public decimal Expences { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expences { get; set; }
 
-            public decimal Total { get; set; }
-
-
+        public decimal Total { get; set; }
 
 
-            public MonthlyLedger()
-            {
 
-            }
+
+        public MonthlyLedger(int year, int month, List<Transaction> transactions)
+        {
+            Year = year;
+            Month = month;
         }
     }
 }
+
--- PetShop/PetShopLibrary/PetReport.cs
+++ PetShop/PetShopLibrary/PetReport.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace PetShopLibrary
 {
+    public enum AnimalType { dog, cat, parrot}
     public interface IPetReport
     {
-        public int Year { get; set; }
-        public int Month { get; set; }
-        public enum AnimalType { dog, cat, parrot }
+        int Year { get; set; }
+        int Month { get; set; }
+        AnimalType Type { get; set; }
 
-        public int TotalSold { get; set; }
+        int TotalSold { get; set; }
     }
 
 
@@ -21,13 +22,14 @@ namespace PetShopLibrary
     {
         public int Year { get; set; }
         public int Month { get; set; }
-        public enum AnimalType { dog, cat, parrot }
-
+        public AnimalType Type { get; set; }
         public int TotalSold { get; set; }
+        
 
-        public PetReport()
+        public PetReport(int year, int month, List<Transaction> transactions)
         {
-
+            Year = year;
+            Month = month;
         }
     }
 }
