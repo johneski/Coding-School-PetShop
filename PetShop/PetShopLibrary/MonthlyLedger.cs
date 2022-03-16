ce1d7c999262ee8c596e3a9898e62ff5c33a733b
Kyriakos Mousias <kmousias1@gmail.com>
2022-03-16 22:16:40 +0200

    New format interface with class in the same cs

--- PetShop/PetShopLibrary/MonthlyLedger.cs
+++ PetShop/PetShopLibrary/MonthlyLedger.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 
 namespace PetShopLibrary
 {
+    interface IMonthlyLedger
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expences { get; set; }
 
-    
-    public class MonthlyLedger: IMonthlyLedger
+        public decimal Total { get; set; }
+
+        public class MonthlyLedger: IMonthlyLedger
     {
         public int Year { get; set; }
         public int Month { get; set; }
--- PetShop/PetShopLibrary/PetReport.cs
+++ PetShop/PetShopLibrary/PetReport.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace PetShopLibrary
 {
-   
+    interface IPetReport
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public enum AnimalType { dog, cat, parrot }
+
+        public int TotalSold { get; set; }
+    }
 
 
 
