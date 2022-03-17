4c0515d94513839f848cbbd8d14a0bfbb59dcbfa
Giannis Eskioglou <johneski65@gmail.com>
2022-03-17 18:25:25 +0200

    PetShop rollback

--- PetShop/PetShopLibrary/Impl/PetShopManager.cs
+++ PetShop/PetShopLibrary/Impl/PetShopManager.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PetShopLibrary.Impl
+namespace PetShopLibrary
 {
-    public class PetReportManager
+    public class PetShopManager
     {
+        private PetShop
     }
 }
