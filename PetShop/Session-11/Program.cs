c8b06cc371518c1697f251784c0926234798a0ae
Giannis Eskioglou <johneski65@gmail.com>
2022-03-18 14:20:48 +0200

    Transaction forms start and petshop logic

--- PetShop/Session-11/Program.cs
+++ PetShop/Session-11/Program.cs
@@ -13,11 +13,6 @@ namespace Session_11
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
 
-            var petShop = new PetShopManager();
-            petShop.Get<Customer>();
-            
-
-
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
