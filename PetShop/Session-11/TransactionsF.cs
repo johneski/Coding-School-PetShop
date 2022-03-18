b787891e51c33c43da6d0304584d91ed6336f047
Giannis Eskioglou <johneski65@gmail.com>
2022-03-18 16:46:18 +0200

    Transaction grid

--- PetShop/Session-11/TransactionsF.cs
+++ PetShop/Session-11/TransactionsF.cs
@@ -13,7 +13,8 @@ namespace Session_11
 {
     public partial class TransactionsF : Form
     {
-        private List<Transaction> _transactions;
+        private PetShopManager _petShop;
+        
         public TransactionsF()
         {
             InitializeComponent();
@@ -21,7 +22,39 @@ namespace Session_11
 
         private void TransactionsF_Load(object sender, EventArgs e)
         {
-            
+            _petShop = new PetShopManager();
+            DummyData();
+            BindingSource bsTransactions = new BindingSource(_petShop.GetTransactions(), null);
+
+            grdTransactions.DataSource = bsTransactions;
+            HideColumns();
+
+
+        }
+
+        private void DummyData()
+        {
+            Transaction t = new Transaction();
+            Transaction t1 = new Transaction();
+            Transaction t2 = new Transaction();
+            Transaction t3 = new Transaction();
+            _petShop.Add(t);
+            _petShop.Add(t1);
+            _petShop.Add(t2);
+            _petShop.Add(t3);
+        }
+
+        private void HideColumns()
+        {
+            grdViewTransactions.Columns["ID"].Visible = false;
+            grdViewTransactions.Columns["CustomerID"].Visible = false;
+            grdViewTransactions.Columns["EmployeeID"].Visible = false;
+            grdViewTransactions.Columns["PetID"].Visible = false;
+            grdViewTransactions.Columns["PetPrice"].Visible = false;
+            grdViewTransactions.Columns["PetFoodID"].Visible = false;
+            grdViewTransactions.Columns["PetFoodQty"].Visible = false;
+            grdViewTransactions.Columns["PetFoodPrice"].Visible = false;
+            grdViewTransactions.Columns["TotalPrice"].Visible = false;
         }
     }
 }
