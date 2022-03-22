9f4a2ee47e7e3fad74c74435af4a84f9841adc81
paoka <paoka@LAPTOP-1016MKVC>
2022-03-22 12:35:11 +0200

    new transactionform

--- PetShop/Session-11/TransactionForm.cs
+++ PetShop/Session-11/TransactionForm.cs
@@ -190,17 +190,6 @@ namespace Session_11
             _petShop.Delete(_currentPet);
             _petShop.DeletePetFoodRange(_currentPet.FoodType.Brand, qty);
             _petShop.Save();
-
-            grvTransaction.RefreshData();
-            MessageBox.Show("Transaction successfully Completed!");
-        }
-
-        private void cmbFoodBrand_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var index = cmbFoodBrand.SelectedIndex;
-            var availableBrands = _petShop.GetAvailableFoodBrands(_currentPet.AnimalType);
-            _currentPet.FoodType.Brand = availableBrands[index];
-            txtFoodPrice.EditValue = _petShop.GetFoodPrice(_currentPet);
         }
     }
 }
